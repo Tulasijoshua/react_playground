376bf240d563091f836e42fe4dd9607617121e85
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-12-05 19:54:47 +0000

    multiple useState

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react'
 
 const State = () => {
     const [count, setCount] = useState(0);
+    const [isEven, setIsEven] = useState(false)
+
   return (
     <div>
         <h1>Count: {count}</h1>
+        <h2>{isEven ? 'Even' : 'Old'}</h2>
         <button onClick={() => setCount(count + 1)}>Increase Count</button>
     </div>
   )
