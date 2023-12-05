25da906f06bc529b05df555eb390424be6f79ee4
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-12-05 19:57:35 +0000

    useState update

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -9,6 +9,7 @@ const State = () => {
         <h1>Count: {count}</h1>
         <h2>{isEven ? 'Even' : 'Old'}</h2>
         <button onClick={() => setCount(count + 1)}>Increase Count</button>
+        <button onClick={() => setIsEven(!isEven)}>Toggle Even/Odd</button>
     </div>
   )
 }
