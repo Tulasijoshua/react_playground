010531051978005960ff6d2041f31f7686397fef
Tulasijoshua <mbawinitulasi12@gmail.com>
2023-12-06 06:34:13 +0000

    useEffect: setting timer

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import './App.css'
 import State from './components/State'
 import Effect from './components/Effect'
+import EffectTwo from './components/EffectTwo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <State />
-      <Effect />
+      {/* <State /> */}
+      {/* <Effect /> */}
+      <EffectTwo />
     </>
   )
 }
---
+++ src/components/EffectTwo.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+const EffectTwo = () => {
+    const [seconds, setSeconds] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setSeconds((prevSeconds) => prevSeconds + 1)
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [])
+  return (
+    <div>
+        <h3>Seconds: {seconds}</h3>
+    </div>
+  )
+}
+
+export default EffectTwo
